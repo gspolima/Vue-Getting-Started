64a280d2e63b74f9c5ed79fa143ebb7efbe4b343
Gustavo Sampaio <sampaioglima@gmail.com>
2022-03-02 16:24:26 -0300

    two-way binding, class binding and key modifiers

--- src/components/heroes.vue
+++ src/components/heroes.vue
@@ -17,37 +17,68 @@
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" />
+                <input class="input" id="firstName" v-model="hero.firstName" />
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" />
+                <input class="input" id="lastName" v-model="hero.lastName" />
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <textarea class="input" id="description" type="text" />
+                <textarea
+                  class="input"
+                  id="description"
+                  type="text"
+                  v-model="hero.description"
+                />
               </div>
               <div class="field">
                 <label class="label">cape color</label>
                 <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" />
+                  <input
+                    type="radio"
+                    id="color-red"
+                    value="red"
+                    v-model="hero.capeColor"
+                    @keyup.esc="clearCapeColor()"
+                  />
                   red
                 </label>
                 <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue" />
+                  <input
+                    type="radio"
+                    id="color-blue"
+                    value="blue"
+                    v-model="hero.capeColor"
+                    @keyup.esc="clearCapeColor()"
+                  />
                   blue
                 </label>
                 <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green" />
+                  <input
+                    type="radio"
+                    id="color-green"
+                    value="green"
+                    v-model="hero.capeColor"
+                    @keyup.esc="clearCapeColor()"
+                  />
                   green
                 </label>
-                <div class="color-line"></div>
+                <div
+                  class="color-line"
+                  :style="{ 'background-color': hero.capeColor }"
+                ></div>
               </div>
               <div class="field">
                 <label for="power">
                   super power
                   <div class="select is-primary">
-                    <select id="power">
+                    <select
+                      id="power"
+                      v-model="hero.power"
+                      @keyup.delete="clearPower"
+                      :class="{ invalid: hero.power === '' }"
+                    >
                       <option disabled value>Please select one</option>
                       <option>Speed</option>
                       <option>Flight</option>
@@ -60,7 +91,12 @@
               <div class="field">
                 <label class="checkbox" for="active">
                   active
-                  <input type="checkbox" class="is-primary" id="active" />
+                  <input
+                    type="checkbox"
+                    class="is-primary"
+                    id="active"
+                    v-model="hero.active"
+                  />
                 </label>
               </div>
             </div>
@@ -110,6 +146,12 @@ export default {
     },
     saveHero() {
       this.message = JSON.stringify(this.hero, null, '\n');
+    },
+    clearPower() {
+      this.hero.power = '';
+    },
+    clearCapeColor() {
+      this.hero.capeColor = '';
     },
   },
 };
